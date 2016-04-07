00274c2743aea0a68736d95419a64eeff2cbcae5
ihs8594 <iqbal.sukur@mavs.uta.edu>
2016-04-06 23:26:01 -0500

    Update p2.s

--- p2.s
+++ p2.s
@@ -77,6 +77,6 @@ _exit:
     SWI 0               @ execute syscall
 
 .data
-read_char:      .ascii      "%c"
+read_char:      .asciz      "%c"
 format_str:     .asciz      "%d"
 print_str:  .asciz "The Result is : %d\n"
