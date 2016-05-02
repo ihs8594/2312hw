ee855443b0ca60e2db852f89d521a60897405fc1
ihs8594 <iqbal.sukur@mavs.uta.edu>
2016-05-01 23:14:53 -0500

    Update arr.s

--- arr.s
+++ arr.s
@@ -19,7 +19,6 @@ writeloop:
     LDR R1, =a              @ get address of a
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
-    ADD R2, R2, #4
     STR R2, [R2]            @ write the address of a[i] to a[i]
     ADD R0, R0, #1          @ increment index
     B   writeloop           @ branch to next loop iteration
